6170e1c6224ec032f74245fcee4defec2f562707
Abe Estrada <abe@estrada.email>
2025-11-23 18:43:36 -0700

    feat(account): display avatar

--- tui/account_view.go
+++ tui/account_view.go
@@ -1,7 +1,10 @@
 package tui
 
 import (
+	"fmt"
+
 	"git.sr.ht/~rockorager/vaxis"
+	"github.com/AbeEstrada/tuit/utils"
 	"github.com/mattn/go-mastodon"
 )
 
@@ -23,7 +26,49 @@ func (v *AccountView) Draw(win vaxis.Window, focused bool, account *mastodon.Acc
 		return
 	}
 
-	win.Println(0, vaxis.Segment{Text: account.Acct})
+	width, _ := win.Size()
+	y := 0
+
+	avatarWidth := 24
+	avatarHeight := 12
+	avatarURL := account.AvatarStatic
+
+	vxImage, cached := utils.ImageCache.Get(avatarURL, avatarWidth, avatarHeight)
+	if cached {
+		if width > avatarWidth {
+			imgWin := win.New(0, y, avatarWidth, avatarHeight)
+			vxImage.Draw(imgWin)
+		}
+	} else {
+		utils.ImageCache.LoadAsync(avatarURL)
+	}
+
+	metaX := avatarWidth + 1
+	metaWin := win.New(metaX, y, width-metaX, avatarHeight)
+	metaWin.Println(
+		0,
+		vaxis.Segment{
+			Text:  account.DisplayName,
+			Style: vaxis.Style{Attribute: vaxis.AttrBold},
+		},
+	)
+	metaWin.Println(
+		1,
+		vaxis.Segment{
+			Text:  fmt.Sprintf("@%s", account.Acct),
+			Style: vaxis.Style{Attribute: vaxis.AttrBold},
+		},
+	)
+	if account.Bot {
+		metaWin.Println(
+			2,
+			vaxis.Segment{
+				Text: "Automated",
+			},
+		)
+	}
+
+	y += avatarHeight
 
 }
 
